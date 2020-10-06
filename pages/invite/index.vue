cd50a0cb4b5e0d6f92901d95184e85c58f1ea78e
Gor Grigoryan <grigoryang085@gmail.com>
2020-10-06 03:24:07 +0300

    Invite.

---
+++ pages/invite/index.vue
@@ -0,0 +1,174 @@
+<template>
+  <section class="invite py-5 px-6">
+    <h4 class="">
+      Инвайты
+    </h4>
+    <v-card
+      class="invite__card mt-8"
+      width="100%"
+      height="15.5rem"
+      color="#6E81A0"
+      @click="() => {}"
+    >
+      <div class="invite__card-title">
+        Инфографика
+      </div>
+    </v-card>
+    <div class="invite__item mt-9">
+      <div class="invite__item-header d-flex align-center justify-space-between">
+        <div class="invite__item-header-left">
+          <p class="my-0">
+            Для респондента:
+          </p>
+        </div>
+        <div class="invite__item-header-right d-flex align-center">
+          <p class="my-0">
+            Доступно:
+          </p>
+          <div class="circle">
+            5
+          </div>
+        </div>
+      </div>
+      <v-row>
+        <v-col
+          cols="8"
+        >
+          <primary-btn @click="dialog = true">
+            Получить ссылку
+          </primary-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <div class="invite__item mt-12">
+      <div class="invite__item-header d-flex align-center justify-space-between">
+        <div class="invite__item-header-left">
+          <p class="my-0">
+            Для друзей:
+          </p>
+        </div>
+        <div class="invite__item-header-right d-flex align-center">
+          <p class="my-0">
+            Доступно:
+          </p>
+          <div class="circle">
+            5
+          </div>
+        </div>
+      </div>
+      <v-row>
+        <v-col
+          cols="8"
+        >
+          <primary-btn @click="dialog = true">
+            Получить ссылку
+          </primary-btn>
+        </v-col>
+      </v-row>
+    </div>
+    <m-dialog
+      :open.sync="dialog"
+      persistent
+    >
+      <div class="dialog">
+        <div
+          class="dialog__link my-5"
+          :style="{color: successCopy ? '#0E7E38' : ''}"
+        >
+          {{ link }}
+        </div>
+        <v-btn
+          class="dialog__btn"
+          color="#334669"
+          height="30"
+          width="100"
+          @click="copyLink"
+        >
+          Скопировать
+        </v-btn>
+      </div>
+    </m-dialog>
+  </section>
+</template>
+
+<script>
+import PrimaryBtn from '../../components/PrimaryBtn'
+import dialog from '../../components/dialog'
+
+export default {
+  name: 'Index',
+  components: {
+    PrimaryBtn,
+    'm-dialog': dialog
+  },
+  layout: 'main',
+  data: () => ({
+    dialog: false,
+    link: 'https://www.hemeya.com/post48485785393498734895sads4a5d8sad4as',
+    successCopy: false
+  }),
+  methods: {
+    copyLink () {
+      navigator.clipboard.writeText(this.link)
+        .then(() => {
+          this.successCopy = true
+        })
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.invite
+  margin-top: 100px
+  margin-bottom: 100px
+  font-family: "Roboto", sans-serif
+  &__card
+    display: flex
+    justify-content: center
+    align-items: center
+    border-radius: 24px
+    color: #E3EFF9
+    font-size: 36px
+  &__item
+    &-header
+      font-size: 16px
+      &-right
+        p
+          color: #979797
+.circle
+  width: 20px
+  height: 20px
+  border-radius: 50%
+  border: 1px solid #6E81A0
+  color: #6E81A0
+  display: flex
+  align-items: center
+  justify-content: center
+  font-size: 16px
+  margin-left: 3px
+
+.dialog
+  height: 160px
+  width: 100%
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+  &__link
+    width: 90%
+    height: 36px
+    line-height: 36px
+    font-size: 16px
+    padding-left: 7px
+    overflow: hidden
+    text-overflow: ellipsis
+    background: #E3EDF7
+    box-shadow: inset -6px -3px 5px rgba(255, 255, 255, 0.750601), inset 3px 6px 6px #C3D7E7, inset 4px 2px 6px #B1C5D5, inset 0px 2px 4px #C8D4E2
+    border-radius: 10px
+    transition: all .3s ease
+  &__btn
+    border-radius: 5px
+    color: #fff
+    text-transform: unset
+</style>
--- pages/profile/index.vue
+++ pages/profile/index.vue
@@ -24,55 +24,148 @@
             <v-progress-circular indeterminate color="blue lighten-2" />
           </v-row>
         </template>
-        <div class="profile__user">
-          <div class="profile__user-avatar">
-            <v-avatar class="mt-3" height="100" width="100">
-              <v-img :src="require(`~/assets/img/${ user.avatar }`)">
-                <template v-slot:placeholder>
-                  <v-row
-                    class="fill-height ma-0"
-                    align="center"
-                    justify="center"
-                  >
-                    <v-progress-circular indeterminate color="blue lighten-2" />
-                  </v-row>
-                </template>
-              </v-img>
-            </v-avatar>
-          </div>
-
-          <v-card
-            class="profile__user-info ma-6 pb-7"
-          >
-            <h1>{{ user.name }}</h1>
-            <p>@{{ user.nickname }}</p>
-            <div class="profile__user-info-data d-flex justify-space-around">
-              <div>
-                <div>{{ user.followers }}</div>
-                Подписчиков
-              </div>
-              <div>
-                <div>{{ user.follows }}</div>
-                Подписок
-              </div>
-              <div>
-                <div>{{ user.publications }}</div>
-                Публикаций
-              </div>
-            </div>
-          </v-card>
-        </div>
       </v-img>
+      <div class="profile__avatar">
+        <v-avatar class="mt-3 " height="100" width="100">
+          <v-img :src="require(`~/assets/img/${ user.avatar }`)">
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular indeterminate color="blue lighten-2" />
+              </v-row>
+            </template>
+          </v-img>
+        </v-avatar>
+
+        <v-btn
+          rounded
+          fab
+          width="30"
+          height="30"
+          class="camera__btn"
+        >
+          <v-icon color="#6E81A0">
+            mdi-camera-outline
+          </v-icon>
+        </v-btn>
+      </div>
+
+      <v-card
+        class="profile__info rounded-xl py-5 px-5"
+      >
+        <div class="d-flex justify-center">
+          <h2>{{ user.name }}</h2>
+          <v-icon
+            color="#6E81A0"
+            class="ml-3"
+          >
+            mdi-pencil-outline
+          </v-icon>
+        </div>
+
+        <p>@{{ user.nickname }}</p>
+        <div class="profile__user-info-data d-flex justify-space-around">
+          <div>
+            <div>{{ user.followers }}</div>
+            Подписчиков
+          </div>
+          <div>
+            <div>{{ user.follows }}</div>
+            Подписок
+          </div>
+          <div>
+            <div>{{ user.publications }}</div>
+            Публикаций
+          </div>
+        </div>
+      </v-card>
     </v-card>
-    <v-card color="#E4F0FA" min-height="50vh" class="profile__card rounded-t-xl">
-      <div class="profile__card-header">
-        dcfgd
+    <v-card
+      color="#E4F0FA"
+      min-height="50vh"
+      class="profile__card rounded-t-xl pt-5 pb-7 px-6"
+    >
+      <div class="profile__card-header d-flex justify-space-between align-center">
+        <div
+          class="profile__card-header-link"
+          :class="{'active-link': activeLink === 0}"
+          @click="activeLink = 0"
+        >
+          Публикации
+        </div>
+        <div
+          class="profile__card-header-link"
+          :class="{'active-link': activeLink === 1}"
+          @click="activeLink = 1"
+        >
+          Избранное
+        </div>
+        <div
+          class="profile__card-header-link"
+          :class="{'active-link': activeLink === 2}"
+          @click="activeLink = 2"
+        >
+          История
+        </div>
+        <v-btn
+          icon
+          fab
+          width="35"
+          height="35"
+          @click="rowView = !rowView"
+        >
+          <v-icon color="#6E81A0">
+            mdi-{{ rowView ? 'apps' : 'view-day' }}
+          </v-icon>
+        </v-btn>
+      </div>
+      <div class="profile__card-posts">
+        <v-row v-if="activeLink === 0">
+          <v-col
+            v-for="(post, index) in posts"
+            :key="index"
+            :cols="rowView ? 12 : 6"
+          >
+            <post-card
+              :small="!rowView"
+              :post="post"
+            />
+          </v-col>
+        </v-row>
+        <v-row v-else-if="activeLink === 1">
+          <v-col
+            v-for="(post, index) in favourites"
+            :key="index"
+            :cols="rowView ? 12 : 6"
+          >
+            <post-card
+              :small="!rowView"
+              :post="post"
+            />
+          </v-col>
+        </v-row>
+        <v-row v-else>
+          <v-col
+            v-for="(post, index) in stories"
+            :key="index"
+            :cols="rowView ? 12 : 6"
+          >
+            <post-card
+              :small="!rowView"
+              :post="post"
+            />
+          </v-col>
+        </v-row>
       </div>
     </v-card>
   </div>
 </template>
 
 <script>
+import postCard from '../../components/postCard'
 const ExpandedCard = () => import('../../components/ExpandedCard')
 require('vue2-animate/dist/vue2-animate.min.css')
 
@@ -80,7 +173,8 @@ export default {
   name: 'Index',
   layout: 'main',
   components: {
-    ExpandedCard
+    ExpandedCard,
+    postCard
   },
   data: () => ({
     user: {
@@ -92,8 +186,21 @@ export default {
       follows: 1486,
       publications: 879
     },
-    fonFlag: false
-  })
+    fonFlag: false,
+    activeLink: 0,
+    rowView: true
+  }),
+  computed: {
+    posts () {
+      return this.$store.getters['publications/getPosts']
+    },
+    favourites () {
+      return this.$store.getters['publications/getPosts']
+    },
+    stories () {
+      return this.$store.getters['publications/getPosts']
+    }
+  }
 }
 </script>
 
@@ -101,15 +208,36 @@ export default {
 .profile
   background: #495B7B
   position: relative
+  &__fon
+    position: relative
   &__card
-    transform: translateY(-8vh)
-  &__user
-    width: 100%
-    height: 100%
-    margin-top: 110px
+    &-header
+      color: #6E81A0
+      &-link
+        font-weight: 700
+        font-size: 16px
+        transition: all .2s ease
+  &__avatar
     text-align: center
-    &-info
-      background: rgba(228, 240, 250, 0.7)
-      border-radius: 24px
+    position: absolute
+    top: 110px
+    left: 50%
+    transform: translate(-50%)
+  &__info
+    text-align: center
+    position: absolute
+    top: 240px
+    left: 50%
+    transform: translate(-50%)
+    background: rgba(228, 240, 250, 0.7)
+    width: 90%
+    color: $mainBlue
 
+.camera__btn
+  position: absolute
+  right: -8px
+  bottom: 8px
+
+.active-link
+  color: #334669 !important
 </style>
